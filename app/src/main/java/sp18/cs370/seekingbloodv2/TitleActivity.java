855f9ef1dd02ea2685129a43c17c975323122a34
mbaello <33732855+mbaello@users.noreply.github.com>
2018-04-10 10:16:16 -0700

    April 10, 10:15am
    Implemented: Fix for TitleActivity and gave it a corresponding XML file.

--- app/src/main/java/sp18/cs370/seekingbloodv2/TitleActivity.java
+++ app/src/main/java/sp18/cs370/seekingbloodv2/TitleActivity.java
@@ -1,15 +1,20 @@
 package sp18.cs370.seekingbloodv2;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
-public class TitleActivity extends Activity {
-    // Declare your buttons here
+public class TitleActivity extends AppCompatActivity {
+    // Variables (like buttons, image views, etc) go here
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Initialize your buttons here (with findViewById)
-
-        // Create onTouchListeners or onClickListeners for your buttons
+        setContentView(R.layout.activity_title);
+        // Your button code goes here
     }
+
 }
