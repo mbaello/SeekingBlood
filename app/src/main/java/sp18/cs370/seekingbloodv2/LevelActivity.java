862647bd6e9677ab3beba1a253e0d699cf5cf8a3
Jenabski <jenab@sonoma.edu>
2018-05-07 18:11:19 -0700

    Test Done Commit

--- app/src/main/java/sp18/cs370/seekingbloodv2/LevelActivity.java
+++ app/src/main/java/sp18/cs370/seekingbloodv2/LevelActivity.java
@@ -1,6 +1,7 @@
 package sp18.cs370.seekingbloodv2;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.gesture.Gesture;
 import android.graphics.BitmapFactory;
 import android.graphics.Rect;
@@ -9,10 +10,14 @@ import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class  LevelActivity extends Activity {
+
+    private Button menu_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +29,23 @@ public class  LevelActivity extends Activity {
         Constants.SCREENHEIGHT = displayMetrics.heightPixels;
         // Set the content view
         setContentView(new Game(this));
+
+        menu_button = (Button) findViewById(R.id.menu_button);
+        menu_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openTitleActivity();
+
+            }
+        });
+    }
+
+    public void openTitleActivity(){
+        Intent intent = new Intent(this, TitleActivity.class);
+        startActivity(intent);
     }
 
 
-
 }
+
+
